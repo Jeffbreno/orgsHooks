337506b0af903b01b36e646c55203d55c0e02f2e
Jefferson Lopes <jeffbreno@gmail.com>
2023-04-20 18:30:43 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Home from './src/view/home';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/view/home/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import Topo from './components/Topo';
+
+export default function Home() {
+  return <Topo />;
+}
