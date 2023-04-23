efde63e120b1d12dfa3d9f8e945e3c04a200f7f6
Jefferson Lopes <jeffbreno@gmail.com>
2023-04-23 20:57:00 -0300

    23042023_2056

--- src/view/home/components/Topo.js
+++ src/view/home/components/Topo.js
@@ -2,16 +2,30 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 
+import {carregaTopo} from '../../../services/carregaDados';
 import logo from '../../../assets/logo.png';
 
-export default function Topo() {
-  return (
+class Topo extends React.Component {
+  state = {
+    topo: {
+      boasVindas: '',
+      legenda: '',
+    },
+  };
+  atualizaTopo() {
+    const retorno = carregaTopo();
+    this.setState({topo: retorno});
+  }
+  componentDidMount() {
+    this.atualizaTopo();
+  }
+  render() {
     <View style={estilos.topo}>
       <Image source={logo} style={estilos.imagem} />
-      <Text style={estilos.boasVindas}>Olá Usuário</Text>
-      <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
-    </View>
-  );
+      <Text style={estilos.boasVindas}>{this.state.topo.boasVindas}</Text>
+      <Text style={estilos.legenda}>{this.state.topo.legenda}</Text>
+    </View>;
+  }
 }
 
 const estilos = StyleSheet.create({
