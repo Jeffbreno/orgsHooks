d5059f0d39db1f56223bed8c9118b6299d73bfc6
Jefferson Lopes <jeffbreno@gmail.com>
2023-04-28 09:21:36 -0300

    28042023_0921

--- src/hooks/useProdutores.js
+++ src/hooks/useProdutores.js
@@ -8,6 +8,17 @@ export default function useProdutores() {
 
   useEffect(() => {
     const retorno = carregaProdutores();
+    retorno.lista.sort((a, b) => {
+      const nameA = a.nome.toUpperCase();
+      const nameB = b.nome.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
     setTitulo(retorno.titulo);
     setLista(retorno.lista);
   }, []);
